1e9204332e83bbfb60f25f53ca18074874022aa0
Chris Thielen <github@sandgnat.com>
2021-07-09 12:08:49 -1000

    feat: better logging

--- packages/server/src/server-main.ts
+++ packages/server/src/server-main.ts
@@ -6,6 +6,7 @@ const app = createHttpServer();
 
 app.listen(PORT, () => {
   /* eslint-disable no-console */
+  console.log(`About to tell you what port it's listening on:`);
   console.log(`Listening on:`);
   console.log(`  http://localhost:${PORT}/ - client only rendering`);
   console.log(`  http://localhost:${PORT}/server - ssr with hydration`);
